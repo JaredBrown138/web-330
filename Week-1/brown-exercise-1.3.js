fbc0b509c1941ffd9f8296ab45978b65271a2e52
Jared Brown <brownjt01@sunyjcc.edu>
2018-02-21 21:29:35 -0500

    Added Header.js and Week 1

---
+++ Week-1/brown-exercise-1.3.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  Exercise 1.3
+; Author: Professor Krasso, Jared Brown 
+; Date:   21 Feburary 2018
+; Modified By: Jared Brown
+; Description: Prints a well formatted list
+; of fruits.
+;===========================================
+*/ 
+
+var header = require('../header.js');
+var headerMsg = header.display("Jared", "Brown", "Exercise 1.3");
+console.log(headerMsg + "\n".repeat(2)); 
