e9bd7c0ed1912f4c461bb2285b1baf181873d8ce
Jared Brown <brownjt01@sunyjcc.edu>
2018-02-25 20:35:57 -0500

    Added Week 1 Exercises

--- Week-1/brown-exercise-1.3.js
+++ Week-1/brown-exercise-1.3.js
@@ -4,11 +4,63 @@
 ; Author: Professor Krasso, Jared Brown 
 ; Date:   21 Feburary 2018
 ; Modified By: Jared Brown
-; Description: Prints a well formatted list
-; of fruits.
+; Description: Quick Review of Object
+; constructors. 
 ;===========================================
 */ 
 
 var header = require('../header.js');
 var headerMsg = header.display("Jared", "Brown", "Exercise 1.3");
 console.log(headerMsg + "\n".repeat(2)); 
+
+const NEWLINE = "\n";
+
+/**
+ * The constructor function for the
+ * CellPhone Object.
+ * 
+ * @param {any} manufacturer 
+ * @param {any} model 
+ * @param {any} color 
+ * @param {any} price 
+ */
+function CellPhone(manufacturer, model, color, price){
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.color = color;
+    this.price = price;
+    
+    /**
+     * Getter method for the price
+     * property
+     * 
+     * @returns {any} this.price
+     */
+    this.getPrice = function(){
+        return this.price;
+    }
+    /**
+     * Getter method for the model
+     * property
+     * 
+     * @returns {any} this.model
+     */
+    this.getModel = function(){
+        return this.model;
+    }
+    /**
+     * Prints out general information
+     * about the phone object
+     * 
+     * @returns void
+     */
+    this.getDetails = function(){
+        var printString = "Manufacturer: " + this.manufacturer + NEWLINE+ "Model: " + this.getModel() +
+            NEWLINE + "Color: " + this.color + NEWLINE + "Price: " + this.getPrice();
+        console.log(printString);    
+    }
+}
+
+var myCellPhone = new CellPhone( "Samsung", "Galaxy Note 4", "Black", "$300");
+myCellPhone.getDetails();
+
